d570aeb6536c5460a4c21cda0d57f84a9bc59af3
--unset <nate@konimbo.co.il>
2017-01-26 13:45:05 +0200

    firebase integration

---
+++ script/db.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyBVdtLDVJqR4Mn5rqw-aZOpBnGrbNNbJLg",
+    databaseURL: "https://konimbo-docs.firebaseio.com",
+    // authDomain: "<YOUR-AUTH-DOMAIN>",
+    // storageBucket: "<YOUR-STORAGE-BUCKET>.appspot.com"
+};
+
+firebase.initializeApp(config);
